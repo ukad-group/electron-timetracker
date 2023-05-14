c654f70dc7b2a4c1fab450951e30685fe6809026
Mykhailo <35040944+mmmykhailo@users.noreply.github.com>
2023-05-14 11:12:40 +0300

    make DateSelector select date

--- renderer/components/DateSelector.tsx
+++ renderer/components/DateSelector.tsx
@@ -1,38 +1,68 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { Dispatch, SetStateAction } from "react";
 
-export default function DateSelector() {
+type DateSelectorProps = {
+  selectedDate: Date;
+  setSelectedDate: Dispatch<SetStateAction<Date>>;
+};
+
+const day = 60 * 60 * 24 * 1000;
+
+export default function DateSelector({
+  selectedDate,
+  setSelectedDate,
+}: DateSelectorProps) {
+  const increaseDate = () => {
+    setSelectedDate((date) => new Date(date.getTime() + day));
+  };
+  const descreaseDate = () => {
+    setSelectedDate((date) => new Date(date.getTime() - day));
+  };
+
   return (
     <section>
-      <div className="bg-white shadow sm:rounded-lg px-4 py-5 sm:px-6 flex justify-between items-center">
+      <div className="flex items-center justify-between px-4 py-5 bg-white shadow sm:rounded-lg sm:px-6">
         <div>
           <h1 className="text-lg font-semibold leading-6 text-gray-900">
             <time dateTime="2022-01-22" className="sm:hidden">
-              Jan 22, 2022
+              {formatDate(selectedDate, "short")}
             </time>
             <time dateTime="2022-01-22" className="hidden sm:inline">
-              January 22, 2022
+              {formatDate(selectedDate, "long")}
             </time>
           </h1>
-          <p className="mt-1 text-sm text-gray-500">Saturday</p>
+          <p className="mt-1 text-sm text-gray-500">
+            {selectedDate?.toLocaleDateString("en-US", { weekday: "long" })}
+          </p>
         </div>
         <div className="flex items-center rounded-md shadow-sm">
           <button
             type="button"
-            className="flex items-center justify-center rounded-l-md border border-r-0 border-gray-300 bg-white py-2 pl-3 pr-4 text-gray-400 hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            className="flex items-center justify-center py-2 pl-3 pr-4 text-gray-400 bg-white border border-r-0 border-gray-300 rounded-l-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            onClick={descreaseDate}
           >
-            <span className="sr-only">Previous month</span>
-            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            <span className="sr-only">Previous day</span>
+            <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
           </button>
-          <span className="relative -mx-px h-5 w-px bg-gray-300" />
+          <span className="relative w-px h-5 -mx-px bg-gray-300" />
           <button
             type="button"
-            className="flex items-center justify-center rounded-r-md border border-l-0 border-gray-300 bg-white py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            className="flex items-center justify-center py-2 pl-4 pr-3 text-gray-400 bg-white border border-l-0 border-gray-300 rounded-r-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            onClick={increaseDate}
           >
-            <span className="sr-only">Next month</span>
-            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            <span className="sr-only">Next day</span>
+            <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
           </button>
         </div>
       </div>
     </section>
   );
+}
+
+function formatDate(date: Date, type: "short" | "long" = "long") {
+  return date.toLocaleDateString("en-US", {
+    month: type,
+    day: "numeric",
+    year: "numeric",
+  });
 }
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,67 +1,71 @@
+import { useState } from "react";
 import DateSelector from "../components/DateSelector";
 import Table from "../components/Table";
 import Header from "../components/Header";
 
 export default function Home() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
-    <>
-      <div className="min-h-full">
-        <Header />
+    <div className="min-h-full">
+      <Header />
 
-        <main className="py-10">
-          <div className="max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
-            <div className="space-y-6 lg:col-start-1 lg:col-span-2">
-              <section>
-                <DateSelector />
-              </section>
-              <section>
-                <div className="bg-white shadow sm:rounded-lg">
-                  <div className="px-4 py-5 sm:px-6">
-                    <Table />
-                  </div>
-                  <div>
-                    <a
-                      href="#"
-                      className="block bg-gray-50 text-sm font-medium text-gray-500 text-center px-4 py-4 hover:text-gray-700 sm:rounded-b-lg"
-                    >
-                      Track more time
-                    </a>
-                  </div>
+      <main className="py-10">
+        <div className="grid max-w-3xl grid-cols-1 gap-6 mx-auto sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
+          <div className="space-y-6 lg:col-start-1 lg:col-span-2">
+            <section>
+              <DateSelector
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+              />
+            </section>
+            <section>
+              <div className="bg-white shadow sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                  <Table />
                 </div>
-              </section>
-            </div>
-
-            <section
-              aria-labelledby="manual-input-title"
-              className="lg:col-start-3 lg:col-span-1"
-            >
-              <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
-                <h2
-                  id="manual-input-title"
-                  className="text-lg font-medium text-gray-900"
-                >
-                  Manual input
-                </h2>
-
-                <textarea
-                  rows={10}
-                  className="mt-3 py-2 px-3 shadow-sm focus-visible:outline-blue-500 block w-full sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={""}
-                  spellCheck={false}
-                />
-                <div className="mt-6 flex flex-col justify-stretch">
-                  <button
-                    type="button"
-                    className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                <div>
+                  <a
+                    href="#"
+                    className="block px-4 py-4 text-sm font-medium text-center text-gray-500 bg-gray-50 hover:text-gray-700 sm:rounded-b-lg"
                   >
-                    Save
-                  </button>
+                    Track more time
+                  </a>
                 </div>
               </div>
             </section>
           </div>
-        </main>
-      </div>
-    </>
+
+          <section
+            aria-labelledby="manual-input-title"
+            className="lg:col-start-3 lg:col-span-1"
+          >
+            <div className="px-4 py-5 bg-white shadow sm:rounded-lg sm:px-6">
+              <h2
+                id="manual-input-title"
+                className="text-lg font-medium text-gray-900"
+              >
+                Manual input
+              </h2>
+
+              <textarea
+                rows={10}
+                className="block w-full px-3 py-2 mt-3 border border-gray-300 rounded-md shadow-sm focus-visible:outline-blue-500 sm:text-sm"
+                defaultValue={""}
+                spellCheck={false}
+              />
+              <div className="flex flex-col mt-6 justify-stretch">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
