7184e918e3a98fabf3aedf43dd10c175f306a5bb
Mykhailo <35040944+mmmykhailo@users.noreply.github.com>
2023-05-24 18:06:08 +0300

    remove redundant default value

--- renderer/components/ManualInputForm.tsx
+++ renderer/components/ManualInputForm.tsx
@@ -25,7 +25,6 @@ export default function ManualInputForm({
         onChange={(e) => setReport(e.target.value)}
         rows={10}
         className="block w-full px-3 py-2 mt-3 border border-gray-300 rounded-md shadow-sm focus-visible:outline-blue-500 sm:text-sm"
-        defaultValue={""}
         spellCheck={false}
       />
       <div className="flex flex-col mt-6 justify-stretch">
