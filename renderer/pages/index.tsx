7df8ab3fee2b1a7fd01133441b79937de67d6bea
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-07-24 11:31:41 +0300

    Parsing error fix

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
   useEffect(() => {
     if (selectedDateReport) {
       const activities = parseReport(selectedDateReport);
-      setSelectedDateActivities(activities.filter((act) => !act.isBreak));
+      setSelectedDateActivities(activities?.filter((act) => !act.isBreak));
       return;
     }
     setSelectedDateActivities([]);
