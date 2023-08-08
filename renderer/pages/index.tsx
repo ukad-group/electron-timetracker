ba430d4f43e8a2d0ee25196e231136d977964f88
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-08-08 16:32:16 +0300

    File autoopeninig

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         setSelectedDateReport(data || "");
       }
     });
-  }, [selectedDate]);
+  }, [selectedDate, reportsFolder]);
 
   useEffect(() => {
     if (selectedDateReport) {
