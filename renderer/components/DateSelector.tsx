d2eb14177b2b84194d25e38873fde2d1881c3889
dima0208 <mashenko0208@gmail.com>
2023-08-15 15:19:33 +0300

    add save and change date shortcut

--- renderer/components/DateSelector.tsx
+++ renderer/components/DateSelector.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
 type DateSelectorProps = {
   selectedDate: Date;
@@ -19,6 +19,21 @@ export default function DateSelector({
     setSelectedDate((date) => new Date(date.getTime() - day));
   };
 
+  const moveOnPressHandler = (e: KeyboardEvent) => {
+    if (e.code === "Arromoveht" && e.ctrlKey) {
+      increaseDate();
+    } else if (e.code === "ArrowLeft" && e.ctrlKey) {
+      descreaseDate();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", moveOnPressHandler);
+    return () => {
+      document.removeEventListener("keydown", moveOnPressHandler);
+    };
+  }, []);
+
   return (
     <div className="flex items-center justify-between px-4 py-5 sm:px-6">
       <div>
@@ -34,24 +49,27 @@ export default function DateSelector({
           {selectedDate?.toLocaleDateString("en-US", { weekday: "long" })}
         </p>
       </div>
-      <div className="flex items-center rounded-md shadow-sm">
-        <button
-          type="button"
-          className="flex items-center justify-center py-2 pl-3 pr-4 text-gray-400 bg-white border border-r-0 border-gray-300 rounded-l-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
-          onClick={descreaseDate}
-        >
-          <span className="sr-only">Previous day</span>
-          <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-        </button>
-        <span className="relative w-px h-5 -mx-px bg-gray-300" />
-        <button
-          type="button"
-          className="flex items-center justify-center py-2 pl-4 pr-3 text-gray-400 bg-white border border-l-0 border-gray-300 rounded-r-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
-          onClick={increaseDate}
-        >
-          <span className="sr-only">Next day</span>
-          <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-        </button>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center rounded-md shadow-sm">
+          <button
+            type="button"
+            className="flex items-center justify-center py-2 pl-3 pr-4 text-gray-400 bg-white border border-r-0 border-gray-300 rounded-l-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            onClick={descreaseDate}>
+            <span className="sr-only">Previous day</span>
+            <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+          </button>
+          <span className="relative w-px h-5 -mx-px bg-gray-300" />
+          <button
+            type="button"
+            className="flex items-center justify-center py-2 pl-4 pr-3 text-gray-400 bg-white border border-l-0 border-gray-300 rounded-r-md hover:text-gray-500 md:w-9 md:px-2 md:hover:bg-gray-50"
+            onClick={increaseDate}>
+            <span className="sr-only">Next day</span>
+            <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+          </button>
+        </div>
+        <span className="block text-xs text-gray-500 text-center">
+          or press ctrl + arrow
+        </span>
       </div>
     </div>
   );
