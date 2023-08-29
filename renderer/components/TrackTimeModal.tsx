7f72a0ae2ac23375181f5ed3272277410d7c94af
Liubomyr <liubomyr.romankiv@ukad-group.com>
2023-08-29 12:29:05 +0300

    35-user-can-accidentelly-rewrite-time issue fix

--- renderer/components/TrackTimeModal.tsx
+++ renderer/components/TrackTimeModal.tsx
@@ -125,6 +125,10 @@ export default function TrackTimeModal({
     setIsValidationEnabled(false);
   };
 
+  const disableTextDrag = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
@@ -207,6 +211,7 @@ export default function TrackTimeModal({
                             isValidationEnabled && !from,
                         }
                       )}
+                      onDragStart = { disableTextDrag }
                     />
                   </div>
 
@@ -233,6 +238,7 @@ export default function TrackTimeModal({
                             isValidationEnabled && !to,
                         }
                       )}
+                      onDragStart = { disableTextDrag }
                     />
                   </div>
 
@@ -252,6 +258,7 @@ export default function TrackTimeModal({
                             isValidationEnabled && (!duration || duration < 0),
                         }
                       )}
+                      onDragStart = { disableTextDrag }
                     />
                   </div>
 
@@ -288,6 +295,7 @@ export default function TrackTimeModal({
                       type="text"
                       tabIndex={4}
                       className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                      onDragStart = { disableTextDrag }
                     />
                   </div>
                 </div>
