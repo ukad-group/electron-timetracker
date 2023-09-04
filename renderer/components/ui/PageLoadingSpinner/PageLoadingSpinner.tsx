afc6bf2e51f22743709cf95d77e5ff2eb3ee17bf
Liubomyr <liubomyr.romankiv@ukad-group.com>
2023-09-04 14:51:45 +0300

    26 pageLoaderSpinner removed

--- renderer/components/ui/PageLoadingSpinner/PageLoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageLoadingSpinner() {
-    return (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-white z-50 flex items-center justify-center">
-            <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin"></div>
-        </div>
-    );
-}
