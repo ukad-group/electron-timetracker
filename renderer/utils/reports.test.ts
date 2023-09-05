58089441d45f5969e4fd398470acac3f8e1948bf
Liubomyr <liubomyr.romankiv@ukad-group.com>
2023-09-05 10:00:45 +0300

    Test fail check

--- renderer/utils/reports.test.ts
+++ renderer/utils/reports.test.ts
@@ -279,7 +279,7 @@ describe("serializeReport function", () => {
     ];
 
     const report: string =
-      "11:30 - timetracker - meeting - calendar discussion\n12:00 - \n";
+      "11:30 - timetracker - meeting - calendar discussion\n";
     expect(serializeReport(activities)).toBe(report);
   });
 
