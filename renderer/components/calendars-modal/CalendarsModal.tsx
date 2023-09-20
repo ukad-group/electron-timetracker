6324c772959048f09921e9a4120482c27895d148
dima0208 <mashenko0208@gmail.com>
2023-09-20 09:37:36 +0300

    remove another calendars panel

--- renderer/components/calendars-modal/CalendarsModal.tsx
+++ renderer/components/calendars-modal/CalendarsModal.tsx
@@ -1,15 +1,19 @@
 import { Fragment, useState } from "react";
-import { Dialog, Disclosure, Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import GoogleCalendarAuth from "../google-calendar/GoogleCalendarAuth";
-import Button from "../ui/Button";
-import { ChevronUpIcon } from '@heroicons/react/20/solid'
+
 export default function CalendarsModal() {
   let [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <button type="button" className="p-2 text-sm text-grey-600 cursor-pointer hover:text-blue-500"
-        onClick={() => {setIsOpen(true)}}>
+      <button
+        type="button"
+        className="p-2 text-sm text-grey-600 cursor-pointer hover:text-blue-500"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
         Calendars
       </button>
       <Transition
@@ -22,7 +26,10 @@ export default function CalendarsModal() {
         leaveTo="transform scale-95 opacity-0"
         as={Fragment}
       >
-        <Dialog onClose={() => setIsOpen(false)} className="fixed inset-0 z-10 overflow-y-auto bg-green-300">
+        <Dialog
+          onClose={() => setIsOpen(false)}
+          className="fixed inset-0 z-10 overflow-y-auto bg-green-300"
+        >
           <Dialog.Panel className="absolute inset-0 z-10 overflow-y-auto bg-white">
             <Dialog.Title>
               <header className="bg-white shadow">
@@ -30,8 +37,23 @@ export default function CalendarsModal() {
                   <div className="flex w-full justify-start items-center flex-shrink min-w-0 gap-4">
                     Calendars Integration
                   </div>
-                  <div className="flex w-full justify-end items-center flex-shrink min-w-0 gap-4" onClick={() => setIsOpen(false)}>
-                    <svg className="h-6 w-6 text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                  <div
+                    className="flex w-full justify-end items-center flex-shrink min-w-0 gap-4"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <svg
+                      className="h-6 w-6 text-gray-500 cursor-pointer"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      {" "}
+                      <line x1="18" y1="6" x2="6" y2="18" />{" "}
+                      <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
                   </div>
                 </div>
               </header>
@@ -39,40 +61,7 @@ export default function CalendarsModal() {
             <div>
               <div className="w-full px-4 pt-16">
                 <div className="mx-auto w-full ounded-2xl bg-white p-2">
-                  <Disclosure>
-                    {({ open }) => (
-                      <>
-                        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-blue-300 px-4 py-2 text-left text-sm font-medium text-blue-600 hover:bg-blue-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                          <span>Google Calendar</span>
-                          <ChevronUpIcon
-                            className={`${
-                              open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-blue-600`}
-                          />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                          <GoogleCalendarAuth/>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                  <Disclosure as="div" className="mt-2">
-                    {({ open }) => (
-                      <>
-                        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-blue-300 px-4 py-2 text-left text-sm font-medium text-blue-600 hover:bg-blue-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                          <span>Another Calendar</span>
-                          <ChevronUpIcon
-                            className={`${
-                              open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-blue-600`}
-                          />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                          No.
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
+                  <GoogleCalendarAuth />
                 </div>
               </div>
             </div>
--- renderer/components/google-calendar/GoogleCalendarAuth.tsx
+++ renderer/components/google-calendar/GoogleCalendarAuth.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { checkIsSignedIn, getEventsList, signIn, signOut } from "../../API/googleCalendarAPI";
+import {
+  checkIsSignedIn,
+  getEventsList,
+  signIn,
+  signOut,
+} from "../../API/googleCalendarAPI";
 import Button from "../ui/Button";
 import useCalendarsContext from "../../hooks/useCalendarsContext";
 
@@ -21,32 +26,45 @@ function GoogleCalendarAuth() {
       setSigned(true);
       getEvents();
     });
-  }
+  };
 
   const signOutHandler = () => {
     setSigned(false);
     signOut();
-  }
+  };
 
   const getEvents = () => {
     getEventsList().then((items) => {
       setEvents(items);
       setGoogleCalendarEvents(items);
     });
-  }
+  };
 
   const buildEventsList = () => {
     return events.map((event) => {
       const { id, start, end, summary, htmlLink } = event;
       const from = setDateTimeObj(start.dateTime);
       const to = setDateTimeObj(end.dateTime);
+
       return (
-        <li id={id} key={id} className="text-m font-medium text-gray-900 px-4 py-2 flex justify-between border items-stretch">
+        <li
+          id={id}
+          key={id}
+          className="text-m font-medium text-gray-900 px-4 py-2 flex justify-between border items-stretch"
+        >
           <div className="w-full justify-start">
             {summary}
-            <span className="block text-sm text-gray-500">{from.date} {from.time} - {to.time}</span>
+            <span className="block text-sm text-gray-500">
+              {from.date} {from.time} - {to.time}
+            </span>
           </div>
-          <a href={ htmlLink } target="_blank" className="flex w-full justify-end align-middle text-sm text-gray-500 pl-10">Go to event</a>
+          <a
+            href={htmlLink}
+            target="_blank"
+            className="flex w-full justify-end align-middle text-sm text-gray-500 pl-10"
+          >
+            Go to event
+          </a>
         </li>
       );
     });
@@ -55,28 +73,29 @@ function GoogleCalendarAuth() {
   const setDateTimeObj = (date) => {
     return {
       date: new Date(date).toLocaleDateString(),
-      time: new Date(date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+      time: new Date(date).toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
     };
-  }
+  };
 
   return (
     <div>
-      {
-        signed
-          ? (
-            <>
-              <div className="flex justify-end" onClick={signOutHandler} >
-                <div className="text-sm font-medium text-blue-600 p-2 cursor-pointer">SignOut</div>
-              </div>
-              <div className="callendar-container">
-                <ul className="callendar-list">
-                  { buildEventsList() }
-                </ul>
-              </div>
-            </>
-          )
-          : <Button text="Connect to Google Calendar" callback={signInHandler} />
-      }
+      {signed ? (
+        <>
+          <div className="flex justify-end" onClick={signOutHandler}>
+            <div className="text-sm font-medium text-blue-600 p-2 cursor-pointer">
+              SignOut
+            </div>
+          </div>
+          <div className="callendar-container">
+            <ul className="callendar-list">{buildEventsList()}</ul>
+          </div>
+        </>
+      ) : (
+        <Button text="Connect to Google Calendar" callback={signInHandler} />
+      )}
     </div>
   );
 }
