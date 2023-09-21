2280b2641f488573d0913e0bb1495202d033b7dc
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-09-21 13:32:44 +0300

    disclosure refactoring

--- renderer/components/ui/DisclosureSection.tsx
+++ renderer/components/ui/DisclosureSection.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import { Disclosure } from "@headlessui/react";
 import React, { ReactNode } from "react";
 
 type DisclosureSectionProps = {
@@ -12,37 +13,40 @@ type DisclosureSectionProps = {
 export default function DisclosureSection(props: DisclosureSectionProps) {
   return (
     <div className="lg:absolute mt-6 w-full">
-      <div
-        className={clsx(
-          "h-16 px-4 py-5 bg-white shadow overflow-hidden transition-all ease-linear duration-300 sm:rounded-lg sm:px-6",
-          {
-            "h-80 overflow-y-auto ": props.isOpen,
-          }
-        )}
-      >
-        <div
-          className="flex justify-between  cursor-pointer"
-          onClick={props.toggleFunction}
-        >
-          <h2
-            id="manual-input-title"
-            className="text-lg font-medium text-gray-900"
-          >
-            {props.title}
-          </h2>
-          <button
+      <Disclosure>
+        {({ open }) => (
+          <div
             className={clsx(
-              "transform transition-transform ease-linear duration-300",
+              "max-h-16 px-4 py-4 bg-white shadow overflow-hidden transition-all ease-linear duration-300 sm:rounded-lg sm:px-6",
               {
-                "rotate-180": props.isOpen,
+                "max-h-80 overflow-y-auto ": open,
               }
             )}
           >
-            <ChevronDownIcon className="w-6 h-7" aria-hidden="true" />
-          </button>
-        </div>
-        {props.children}
-      </div>
+            <Disclosure.Button className=" w-full">
+              <div className="flex justify-between cursor-pointer">
+                <h2
+                  id="manual-input-title"
+                  className="text-lg font-medium text-gray-900"
+                >
+                  {props.title}
+                </h2>
+                <button
+                  className={clsx(
+                    "transform transition-transform ease-linear duration-300",
+                    {
+                      "rotate-180": open,
+                    }
+                  )}
+                >
+                  <ChevronDownIcon className="w-6 h-7" aria-hidden="true" />
+                </button>
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel static>{props.children}</Disclosure.Panel>
+          </div>
+        )}
+      </Disclosure>
     </div>
   );
 }
