37519156bd012b924a3a58afb62133315576bfe3
Artur.Nahaitsev <artur.nahaitsev@ukad-group.com>
2023-10-05 21:59:46 +0300

    Updated Settings page

--- renderer/components/google-calendar/GoogleCalendarAuth.tsx
+++ renderer/components/google-calendar/GoogleCalendarAuth.tsx
@@ -57,74 +57,52 @@ function GoogleCalendarAuth() {
       .catch((e) => console.log(e));
   };
 
-  const buildEventsList = () => {
-    return googleEvents.map((event) => {
-      const { id, start, end, summary, htmlLink } = event;
-      const from = setDateTimeObj(start.dateTime);
-      const to = setDateTimeObj(end.dateTime);
+  // const buildEventsList = () => {
+  //   return googleEvents.map((event) => {
+  //     const { id, start, end, summary, htmlLink } = event;
+  //     const from = setDateTimeObj(start.dateTime);
+  //     const to = setDateTimeObj(end.dateTime);
 
-      return (
-        <li
-          id={id}
-          key={id}
-          className="text-m font-medium text-gray-900 px-4 py-2 flex justify-between border items-stretch"
-        >
-          <div className="w-full justify-start">
-            {summary ? summary : "No title"}
-            <span className="block text-sm text-gray-500">
-              {from.date} {from.time} - {to.time}
-            </span>
-          </div>
-          <a
-            href={htmlLink}
-            target="_blank"
-            className="flex w-full justify-end align-middle text-sm text-gray-500 pl-10"
-          >
-            Go to event
-          </a>
-        </li>
-      );
-    });
-  };
+  //     return (
+  //       <li
+  //         id={id}
+  //         key={id}
+  //         className="text-m font-medium text-gray-900 px-4 py-2 flex justify-between border items-stretch"
+  //       >
+  //         <div className="w-full justify-start">
+  //           {summary ? summary : "No title"}
+  //           <span className="block text-sm text-gray-500">
+  //             {from.date} {from.time} - {to.time}
+  //           </span>
+  //         </div>
+  //         <a
+  //           href={htmlLink}
+  //           target="_blank"
+  //           className="flex w-full justify-end align-middle text-sm text-gray-500 pl-10"
+  //         >
+  //           Go to event
+  //         </a>
+  //       </li>
+  //     );
+  //   });
+  // };
 
-  const setDateTimeObj = (date) => {
-    return {
-      date: new Date(date).toLocaleDateString(),
-      time: new Date(date).toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-    };
-  };
+  // const setDateTimeObj = (date) => {
+  //   return {
+  //     date: new Date(date).toLocaleDateString(),
+  //     time: new Date(date).toLocaleTimeString([], {
+  //       hour: "2-digit",
+  //       minute: "2-digit",
+  //     }),
+  //   };
+  // };
 
   return (
-    <div>
+    <div className="flex-shrink-0">
       {isLogged ? (
-        <>
-          <Button
-            text="Signout from google"
-            callback={signOutHandler}
-            type="button"
-          />
-          <div className="flex justify-between">
-            <h3 className="text-lg font-semibold py-4">
-              List of google events for today
-            </h3>
-          </div>
-          <div className="callendar-container">
-            {googleEvents.length === 0 ? (
-              <p>There are no events for today</p>
-            ) : (
-              <ul className="callendar-list">{buildEventsList()}</ul>
-            )}
-          </div>
-        </>
+        <Button text="Sign Out" callback={signOutHandler} type="button" />
       ) : (
-        <Button
-          text="Connect to Google Calendar"
-          callback={signInHandler}
-          type="button"
-        />
+        <Button text="Sign In" callback={signInHandler} type="button" />
       )}
     </div>
   );
--- renderer/pages/settings.tsx
+++ renderer/pages/settings.tsx
@@ -1,25 +1,177 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { shallow } from "zustand/shallow";
+import { getTrelloAuthUrl } from "../API/trelloAPI";
+import FolderSelector from "../components/FolderSelector";
 import GoogleCalendarAuth from "../components/google-calendar/GoogleCalendarAuth";
+import Button from "../components/ui/Button";
+import { useGoogleCalendarStore } from "../store/googleCalendarStore";
+import { useMainStore } from "../store/mainStore";
 
-const SettingsPage = () => (
-  <div className="mx-auto sm:px-6 lg:max-w-[1400px] bg-white shadow-md p-6 rounded-lg">
-    <h1 className="text-4xl mb-8">Settings page</h1>
-    <Link
-      className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium border rounded-md shadow-sm mb-4 bg-blue-600 text-white hover:bg-blue-700"
-      href="/"
-    >
-      Go home
-    </Link>
-    <div>
-      <Link
-        className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium border rounded-md shadow-sm mb-4 bg-blue-600 text-white hover:bg-blue-700"
-        href="/trello"
-      >
-        Connect to trello
-      </Link>
+const TRELLO_KEY = process.env.NEXT_PUBLIC_TRELLO_KEY;
+const RETURN_URL = process.env.NEXT_PUBLIC_TRELLO_REDIRECT_URI;
+
+function extractTokenFromString(inputString: string) {
+  const parts = inputString.split("#");
+
+  if (parts.length >= 2) {
+    const afterHash = parts[1];
+    const tokenPart = afterHash.split("=");
+
+    if (tokenPart.length === 2 && tokenPart[0] === "token") {
+      return tokenPart[1];
+    }
+  }
+
+  return "";
+}
+
+const SettingsPage = () => {
+  const [token, setToken] = useState("");
+  const router = useRouter();
+  const [reportsFolder, setReportsFolder] = useMainStore(
+    (state) => [state.reportsFolder, state.setReportsFolder],
+    shallow
+  );
+  const { isLogged } = useGoogleCalendarStore();
+
+  const handleSignInTrelloButton = () => {
+    const trelloAuthUrl = getTrelloAuthUrl({
+      key: TRELLO_KEY,
+      returnUrl: RETURN_URL,
+    });
+
+    router.push(trelloAuthUrl);
+  };
+
+  const handleSignOutTrelloButton = () => {
+    localStorage.removeItem("trelloToken");
+    setToken("");
+    router.push("/settings");
+  };
+
+  useEffect(() => {
+    if (window.location.hash.includes("token")) {
+      const tokenFromUrl = extractTokenFromString(window.location.hash);
+
+      localStorage.setItem("trelloToken", tokenFromUrl);
+      setToken(tokenFromUrl);
+    }
+
+    if (Boolean(localStorage.getItem("trelloToken"))) {
+      const storedToken = localStorage.getItem("trelloToken") as string;
+
+      setToken(storedToken);
+    }
+  }, []);
+
+  return (
+    <div className="mx-auto sm:px-6 max-w-3xl flex flex-col gap-6 px-6 py-10">
+      <section>
+        <div className="bg-white shadow sm:rounded-lg p-6 flex items-center justify-between">
+          <span className="text-lg font-medium text-gray-900">Settings</span>
+          <Link href="/">
+            <div className="flex justify-end items-center flex-shrink min-w-0 gap-4">
+              <XMarkIcon
+                className="w-6 h-6 cursor-pointer"
+                aria-hidden="true"
+              />
+            </div>
+          </Link>
+        </div>
+      </section>
+      <section>
+        <div className="bg-white shadow sm:rounded-lg p-6 flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <span className="text-lg font-medium text-gray-900">
+              Folder with reports
+            </span>
+            <p className="text-sm text-gray-500">
+              Specify the path on your computer where your reports will be saved
+            </p>
+          </div>
+          <div className="flex w-full items-center flex-shrink min-w-0 gap-4">
+            <FolderSelector
+              folderLocation={reportsFolder}
+              setFolderLocation={setReportsFolder}
+            />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="bg-white shadow sm:rounded-lg p-6 flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <span className="text-lg font-medium text-gray-900">
+              Connections
+            </span>
+            <p className="text-sm text-gray-500">
+              You can connect available resources to use their capabilities to
+              complete your reports
+            </p>
+          </div>
+          <div className="p-4 flex items-start justify-between gap-6 border rounded-lg shadow">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-4">
+                <span>Trello</span>
+
+                {token.length > 0 ? (
+                  <div className="text-green-500 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100">
+                    Already authorized
+                  </div>
+                ) : (
+                  <div className="text-red-500 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100">
+                    Not authorized
+                  </div>
+                )}
+              </div>
+              <p className="text-sm text-gray-500">
+                After connection, you will be able to fill in the Description
+                field with tasks from the drop-down list
+              </p>
+            </div>
+            <div className="flex-shrink-0">
+              {token.length > 0 ? (
+                <Button
+                  text="Sign Out"
+                  callback={handleSignOutTrelloButton}
+                  type="button"
+                />
+              ) : (
+                <Button
+                  text="Sign In"
+                  callback={handleSignInTrelloButton}
+                  type="button"
+                />
+              )}
+            </div>
+          </div>
+          <div className="p-4 flex items-start justify-between gap-6 border rounded-lg shadow">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-4">
+                <span>Google</span>
+                {isLogged ? (
+                  <div className="text-green-500 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100">
+                    Already authorized
+                  </div>
+                ) : (
+                  <div className="text-red-500 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100">
+                    Not authorized
+                  </div>
+                )}
+              </div>
+              <p className="text-sm text-gray-500">
+                After connection, you will be able to fill in the Report with
+                the information from events of your Google Calendar
+              </p>
+            </div>
+            <GoogleCalendarAuth />
+          </div>
+        </div>
+      </section>
     </div>
-    <GoogleCalendarAuth />
-  </div>
-);
+  );
+};
 
 export default SettingsPage;
