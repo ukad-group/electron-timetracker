85c0a02ab7667b9154e8c4a5124ae2a295b12f55
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-11-27 17:28:11 +0200

    minor

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -388,34 +388,32 @@ export default function Home() {
 
   return (
     <div className="h-full bg-gray-100 dark:bg-dark-back">
-      {true && (
-        <span
-          title={
-            isDropboxConnected
-              ? "Dropbox is connected"
-              : "Dropbox is not connected"
-          }
+      <span
+        title={
+          isDropboxConnected
+            ? "Dropbox is connected"
+            : "Dropbox is not connected"
+        }
+      >
+        <svg
+          className="p-2 "
+          fill={false ? "#007FE6" : "#64748b"}
+          height="35px"
+          width="35px"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-271 282 256 238"
         >
-          <svg
-            className="p-2 "
-            fill={false ? "#007FE6" : "#64748b"}
-            height="35px"
-            width="35px"
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="-271 282 256 238"
-          >
-            <g>
-              <polygon points="-271,414.5 -195.7,463.6 -143,419.7 -218.9,372.8 	" />
-              <polygon points="-195.7,282 -271,331.1 -218.9,372.8 -143,325.9 	" />
-              <polygon points="-15,331.1 -90.3,282 -143,325.9 -67.1,372.8 	" />
-              <polygon points="-143,419.7 -90.3,463.6 -15,414.5 -67.1,372.8 	" />
-              <polygon points="-142.8,429.1 -195.7,473 -218.3,458.2 -218.3,474.8 -142.8,520 -67.4,474.8 -67.4,458.2 -90,473 	" />
-            </g>
-          </svg>
-        </span>
-      )}
+          <g>
+            <polygon points="-271,414.5 -195.7,463.6 -143,419.7 -218.9,372.8 	" />
+            <polygon points="-195.7,282 -271,331.1 -218.9,372.8 -143,325.9 	" />
+            <polygon points="-15,331.1 -90.3,282 -143,325.9 -67.1,372.8 	" />
+            <polygon points="-143,419.7 -90.3,463.6 -15,414.5 -67.1,372.8 	" />
+            <polygon points="-142.8,429.1 -195.7,473 -218.3,458.2 -218.3,474.8 -142.8,520 -67.4,474.8 -67.4,458.2 -90,473 	" />
+          </g>
+        </svg>
+      </span>
       <VersionMessage />
       <main className="py-10">
         <div className="grid max-w-3xl grid-cols-1 gap-6 mx-auto sm:px-6 lg:max-w-[1400px] lg:grid-cols-[31%_31%_auto]">
