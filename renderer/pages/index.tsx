a98ccc05ee95773a1982c7309ea36a72133c55ff
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-12-13 14:23:04 +0200

    commented

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -343,12 +343,12 @@ export default function Home() {
           tempActivities.push(...selectedDateActivities);
           break;
         }
-        if (newActFrom === indexActFrom) {
-          tempActivities.push(activity);
-          isPastTime = true;
-          activity.isValid = true;
-          continue;
-        }
+        // if (newActFrom === indexActFrom) {
+        //   tempActivities.push(activity);
+        //   isPastTime = true;
+        //   activity.isValid = true;
+        //   continue;
+        // }
       } catch (err) {
         global.ipcRenderer.send(
           "front error",
