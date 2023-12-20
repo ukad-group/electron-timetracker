a65dc20628912f5d628f64b21d75a864f1a9b4b2
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-12-20 12:25:23 +0200

    small redesign

--- renderer/components/ui/ButtonTransparent.tsx
+++ renderer/components/ui/ButtonTransparent.tsx
@@ -12,7 +12,7 @@ export default function ButtonTransparent({
   return (
     <button
       onClick={callback}
-      className="inline-flex items-center justify-center px-4 py-2 gap-2 text-sm font-medium rounded-md border shadow-sm dark:border-dark-form-border hover:underline text-gray-500 dark:text-dark-main"
+      className="inline-flex items-center justify-center pl-2 pr-4 py-2 gap-2 text-sm font-medium rounded-md border shadow-sm dark:border-dark-form-border  dark:hover:bg-dark-button-back-gray text-gray-500 dark:text-dark-main"
     >
       {children}
     </button>
