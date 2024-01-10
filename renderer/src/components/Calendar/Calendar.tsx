86290e5acbd4c6c35d964834c3396134af32f6aa
Artur.Nahaitsev <artur.nahaitsev@ukad-group.com>
2024-01-10 19:26:01 +0200

    Changed position of ExclamationCircleIcon in Calendar component

--- renderer/src/components/Calendar/Calendar.tsx
+++ renderer/src/components/Calendar/Calendar.tsx
@@ -371,7 +371,7 @@ function renderEventContent(eventInfo) {
   return (
     <>
       {eventInfo.event.extendedProps.isValid === false && (
-        <ExclamationCircleIcon className="w-5 h-5 absolute fill-red-500 bottom-[26px] -left-[1px] dark:fill-red-500/70" />
+        <ExclamationCircleIcon className="w-5 h-5 absolute fill-red-500 -top-[25px] -left-[1px] dark:fill-red-500/70" />
       )}
       {eventInfo.event.extendedProps.workDurationMs ? (
         <p className="whitespace-normal">
