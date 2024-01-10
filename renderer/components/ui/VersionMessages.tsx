2961dbe53c77fbbcb2fd513cbd100fdd4c4081a7
Artur.Nahaitsev <artur.nahaitsev@ukad-group.com>
2024-01-10 12:54:52 +0200

    Added close button to update message

--- renderer/components/ui/VersionMessages.tsx
+++ renderer/components/ui/VersionMessages.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
-import { CheckIcon, ExclamationCircleIcon } from "@heroicons/react/24/solid";
+import {
+  CheckIcon,
+  ExclamationCircleIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 import { UpdateInfo } from "electron-updater";
 
 export default function VersionMessage() {
   const [isUpdate, setIsUpdate] = useState(false);
   const [isDownload, setIsDownload] = useState(false);
   const [version, setVersion] = useState("");
+  const storedVersionData = JSON.parse(
+    localStorage.getItem("version-data")
+  ) || { version: "", showMessage: true };
+  const [showMessage, setShowMessage] = useState(storedVersionData.showMessage);
 
   useEffect(() => {
     global.ipcRenderer.on(
@@ -22,11 +30,14 @@ export default function VersionMessage() {
         setVersion(info.version);
       }
     );
+
     (async () => {
       const [updateStatus, currentVersion] = await global.ipcRenderer.invoke(
         "app:update-status"
       );
+
       setVersion(currentVersion);
+
       switch (updateStatus) {
         case "available":
           setIsUpdate(true);
@@ -36,26 +47,46 @@ export default function VersionMessage() {
           break;
       }
     })();
+
     return () => {
       global.ipcRenderer.removeAllListeners("update-available");
       global.ipcRenderer.removeAllListeners("downloaded");
     };
   }, []);
 
+  useEffect(() => {
+    if (version.length > 0 && version !== storedVersionData.version) {
+      storedVersionData.version = version;
+      storedVersionData.showMessage = true;
+
+      localStorage.setItem("version-data", JSON.stringify(storedVersionData));
+    }
+
+    setShowMessage(storedVersionData.showMessage);
+  }, [version]);
+
   const install = () => {
     global.ipcRenderer.send("install");
   };
+
+  const closeBtnHandler = () => {
+    storedVersionData.showMessage = false;
+    localStorage.setItem("version-data", JSON.stringify(storedVersionData));
+
+    setShowMessage(storedVersionData.showMessage);
+  };
+
   return (
     <div className="flex justify-center pt-2">
-      <div className="flex gap-2 items-center flex-shrink-0 px-2 lg:px-0">
-        {isUpdate && !isDownload && (
+      <div className="flex gap-2 items-center px-2 lg:px-0">
+        {isUpdate && !isDownload && showMessage && (
           <span className="flex gap-2 items-center rounded-lg px-3 py-2 bg-blue-300 text-blue-800 dark:text-blue-500 dark:bg-blue-400/30">
             <ExclamationCircleIcon className="w-6 h-6 fill-blue-400 dark:fill-blue-400/30 stroke-white bg:stroke-gray-300" />
             The version {version} is loading
           </span>
         )}
-        {isDownload && (
-          <span className="flex gap-2 items-center rounded-lg px-3 py-2 text-sm bg-green-300 text-green-800 dark:bg-green-300/30 dark:text-green-400/70">
+        {isDownload && showMessage && (
+          <span className="relative flex gap-2 items-center rounded-lg px-12 py-3 text-sm bg-green-300 text-green-800 dark:bg-green-300/30 dark:text-green-400/70">
             <CheckIcon className="w-6 h-6 fill-green-800 dark:fill-green-400/70" />
             <p className="whitespace-normal">
               New version {version} is downloaded. Turn off the app, or click
@@ -67,6 +98,10 @@ export default function VersionMessage() {
             >
               Install
             </button>
+            <XMarkIcon
+              className="w-6 h-6 fill-green-800 dark:fill-green-400/70 absolute right-1 top-1 cursor-pointer"
+              onClick={closeBtnHandler}
+            />
           </span>
         )}
       </div>
