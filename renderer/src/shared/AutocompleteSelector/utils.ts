6fa6901465ff3fc8f1dee5170b5e869e3909a129
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2024-02-02 14:21:54 +0200

    remove dublicate

--- renderer/src/shared/AutocompleteSelector/utils.ts
+++ renderer/src/shared/AutocompleteSelector/utils.ts
@@ -2,40 +2,36 @@ export const filterList = ({
   selectedItem,
   availableItems,
   additionalItems,
-  showedSuggestionsNumber
+  showedSuggestionsNumber,
 }) =>
   selectedItem === ""
-  ? availableItems &&
-  [...availableItems]
-    .concat(additionalItems ? additionalItems : [])
-    .filter((activity, i) => {
-      if (showedSuggestionsNumber) {
-        return activity !== "" && i < showedSuggestionsNumber;
-      }
-      return activity !== "";
-    })
-  : availableItems &&
-  [...availableItems]
-    .sort()
-    .concat(additionalItems ? additionalItems : [])
-    .reduce((accumulator, current) => {
-      let duplicate = false;
-      if (current.startsWith("TT:: ") || current.startsWith("JI:: ")) {
-        duplicate = availableItems.includes(current.slice(5));
-      }
-      if (
-        !duplicate &&
-        current.toLowerCase() === selectedItem.toLowerCase()
-      ) {
-        accumulator.unshift(current);
-      } else if (
-        !duplicate &&
-        current
-          .toLowerCase()
-          .includes((selectedItem || "").toLowerCase())
-      ) {
-        accumulator.push(current);
-      }
-      return accumulator;
-    }, [])
-    .slice(0, 15);
+    ? availableItems &&
+      [...availableItems]
+        .concat(additionalItems ? additionalItems : [])
+        .filter((activity, i) => {
+          if (showedSuggestionsNumber) {
+            return activity !== "" && i < showedSuggestionsNumber;
+          }
+          return activity !== "";
+        })
+    : availableItems &&
+      [...availableItems]
+        .sort()
+        .concat(additionalItems ? additionalItems : [])
+        .reduce((accumulator, current) => {
+          let duplicate = false;
+          if (current.startsWith("TT:: ") || current.startsWith("JI:: ")) {
+            duplicate = availableItems.includes(current.slice(5));
+          }
+          if (
+            !duplicate &&
+            current
+              .toLowerCase()
+              .includes((selectedItem || "").toLowerCase()) &&
+            current.toLowerCase() !== selectedItem.toLowerCase()
+          ) {
+            accumulator.push(current);
+          }
+          return accumulator;
+        }, [])
+        .slice(0, 15);
