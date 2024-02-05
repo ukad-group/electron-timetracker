952b5e8c27b178442d45e094bf1ae0519e9db66c
Oleh Khmelevskyi <oleh.khmelevskyi@ukad-group.com>
2024-02-05 13:19:55 +0200

    fix tooltip unit test

--- renderer/src/shared/Tooltip/__tests__/Tooltip.test.tsx
+++ renderer/src/shared/Tooltip/__tests__/Tooltip.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Tooltip from '../Tooltip';
-import { CONNECTION_MESSAGE } from '@/helpers/contstants';
 
 describe('Tooltip Component', () => {
   test('should render without errors', () => {
@@ -13,15 +12,5 @@ describe('Tooltip Component', () => {
     fireEvent.click(tooltipWrapper);
 
     expect("Copied").toBeDefined();
-  });
-
-  test('handles mouse enter and leave events', async () => {
-    render(<Tooltip tooltipText={CONNECTION_MESSAGE}>Test</Tooltip>);
-    const tooltipWrapper = screen.getByTestId('hover-tooltip-test-id');
-    fireEvent.mouseEnter(tooltipWrapper);
-
-    expect(CONNECTION_MESSAGE).toBeDefined();
-
-    fireEvent.mouseLeave(tooltipWrapper);
   });
 });
