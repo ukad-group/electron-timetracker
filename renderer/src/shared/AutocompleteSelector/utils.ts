1d632c898147822bed4adc95503e1b55bf0366ca
Oleksii Mehlii <133204916+o-mehlii@users.noreply.github.com>
2024-02-09 15:51:09 +0200

    Feature/relevant sorting (#232)
    * more relevant sorting
    
    * tests

--- renderer/src/shared/AutocompleteSelector/utils.ts
+++ renderer/src/shared/AutocompleteSelector/utils.ts
@@ -3,35 +3,51 @@ export const filterList = ({
   availableItems,
   additionalItems,
   showedSuggestionsNumber,
-}) =>
-  selectedItem === ""
-    ? availableItems &&
-      [...availableItems]
-        .concat(additionalItems ? additionalItems : [])
-        .filter((activity, i) => {
-          if (showedSuggestionsNumber) {
-            return activity !== "" && i < showedSuggestionsNumber;
-          }
-          return activity !== "";
-        })
-    : availableItems &&
-      [...availableItems]
-        .sort()
-        .concat(additionalItems ? additionalItems : [])
-        .reduce((accumulator, current) => {
-          let duplicate = false;
-          if (current.startsWith("TT:: ") || current.startsWith("JI:: ")) {
-            duplicate = availableItems.includes(current.slice(5));
-          }
-          if (
-            !duplicate &&
-            current
-              .toLowerCase()
-              .includes((selectedItem || "").toLowerCase()) &&
-            current.toLowerCase() !== selectedItem.toLowerCase()
-          ) {
-            accumulator.push(current);
-          }
-          return accumulator;
-        }, [])
-        .slice(0, 15);
+}) => {
+  const mostRelevantItems = [];
+
+  const relevantItems =
+    selectedItem === ""
+      ? availableItems &&
+        [...availableItems]
+          .concat(additionalItems ? additionalItems : [])
+          .filter((activity, i) => {
+            if (showedSuggestionsNumber) {
+              return activity !== "" && i < showedSuggestionsNumber;
+            }
+            return activity !== "";
+          })
+      : availableItems &&
+        [...availableItems]
+          .sort()
+          .concat(additionalItems ? additionalItems : [])
+          .reduce((accumulator, current) => {
+            let duplicate = false;
+            if (current.startsWith("TT:: ") || current.startsWith("JI:: ")) {
+              duplicate = availableItems.includes(current.slice(5));
+            }
+            if (
+              !duplicate &&
+              current
+                .toLowerCase()
+                .includes((selectedItem || "").toLowerCase()) &&
+              current.toLowerCase() !== selectedItem.toLowerCase()
+            ) {
+              if (
+                current
+                  .toLowerCase()
+                  .split("")
+                  .slice(0, selectedItem.length)
+                  .join("") == selectedItem
+              ) {
+                mostRelevantItems.unshift(current);
+              } else {
+                accumulator.push(current);
+              }
+            }
+            return accumulator;
+          }, [])
+          .slice(0, 15);
+
+  return mostRelevantItems.reverse().concat(relevantItems);
+};
